6c01b1cf6fd56e0e319fe024f6bea367397c1e91
Tyler Band <tylrm007@gmail.com>
2015-03-20 13:32:37 -0400

    Renamed icons (removed old) added URL fixup and replacement, found bug in JSON

--- AzureMarketRESTService/ProxyHandler.cs
+++ AzureMarketRESTService/ProxyHandler.cs
@@ -2,30 +2,27 @@
 using System.Web;
 using System.Net.Http;
 using System.Configuration;
+using System.Diagnostics;
 
 namespace AzureMarketRESTService
 {
     public class ProxyHandler : DelegatingHandler
     {
-        /// <summary>
-        /// You will need to configure this handler in the Web.config file of your 
-        /// web and register it with IIS before being able to use it. For more information
-        /// see the following link: http://go.microsoft.com/?linkid=8101007
-        /// </summary>
-         
-
+        
         protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
         {
             string remoteWebSite = ConfigurationManager.AppSettings["EspressoURL"];
-            
             string apikey = ConfigurationManager.AppSettings["APIKey"];
-            string urlPart = parseURL(request);//request remove everything after /api/
+
+            UriBuilder remoteURI = new UriBuilder(remoteWebSite);
+            UriBuilder originalURI = new UriBuilder(request.RequestUri);            
+            string urlPart = parseURL(request.RequestUri.PathAndQuery);
             UriBuilder forwardUri = new UriBuilder(remoteWebSite + urlPart);
-            //strip off the proxy port and replace with an Http port
             forwardUri.Port = 80;
-            //send it on to the requested URL
+            
             request.RequestUri = forwardUri.Uri;
             HttpClient client = new HttpClient();
+            request.Headers.Host = remoteURI.Host;
             request.Headers.Add("Authorization", string.Format("Espresso {0}", apikey + ":1"));
             if (request.Method == HttpMethod.Get)
             {
@@ -33,19 +30,41 @@ namespace AzureMarketRESTService
             }
             var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
             response.EnsureSuccessStatusCode();
-     
+            if (response.IsSuccessStatusCode)
+            {
+                await stripOutURLLinks(response, remoteURI, originalURI);
+            }
+      
             return response;
         }
 
-        private string parseURL(HttpRequestMessage request)
+        private async System.Threading.Tasks.Task<HttpResponseMessage> stripOutURLLinks(HttpResponseMessage response, UriBuilder remoteURI, UriBuilder originalURI)
         {
-            string url =  request.RequestUri.OriginalString;
-            int index = url.IndexOf("/api/");
-            if(index > 0){
-                url = url.Substring(index + 5);
+            string content = await response.Content.ReadAsStringAsync();
+            content = content.Replace(remoteURI.Uri.Authority, originalURI.Uri.Authority);
+            content = content.Replace(remoteURI.Path, "/api/");
+            string path = remoteURI.Path.Replace("/v1/", "");
+            content = content.Replace(path, "/api");
+            var oldHeaders = response.Content.Headers;
+
+            response.Content = new StringContent(content);
+            return response;
+        }
+
+        private string parseURL(string pathAndQuery)
+        {
+            string url = "";
+            int index = pathAndQuery.IndexOf("/api/");
+            if (index == 0)
+            {
+                url = pathAndQuery.Substring(index + 5);
+            }
+            else
+            {
+                throw new Exception("missing /api/{resource|tabl|view}[?{params}]");
             }
             return url;
         }
     }
-    
+
 }
