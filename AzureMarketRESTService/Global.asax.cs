ea3f60a04c2f127ef3441934016779b9d98641ee
Tyler Band <tylrm007@gmail.com>
2015-03-20 22:17:23 -0400

    added additional logon parameters

--- AzureMarketRESTService/Global.asax.cs
+++ AzureMarketRESTService/Global.asax.cs
@@ -13,16 +13,17 @@ namespace AzureMarketRESTService
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            ConfigurationManager.AppSettings["ServerName"] = "espressologic-msftdemo.cloudapp.net";
-            ConfigurationManager.AppSettings["AccountName"] = "msftdemo";
-            ConfigurationManager.AppSettings["ProjectName"] = "demo";
-            ConfigurationManager.AppSettings["DefaultAPIVersion"] = "v1";
+            ConfigurationManager.AppSettings["ServerName"] = "";
+            ConfigurationManager.AppSettings["AccountName"] = "";
+            ConfigurationManager.AppSettings["ProjectName"] = "";
+            ConfigurationManager.AppSettings["DefaultAPIVersion"] = "";
             ConfigurationManager.AppSettings["UserName"] = "";
             ConfigurationManager.AppSettings["Password"] = "";
             ConfigurationManager.AppSettings["UseAPIKey"] = "true";
-            //ConfigurationManager.AppSettings["EspressoURL"] = "http://espressologic-msftdemo.cloudapp.net/rest/msftdemo/demo/v1/";
+            //ConfigurationManager.AppSettings["EspressoURL"] = "";
+            //"http://espressologic-msftdemo.cloudapp.net/rest/msftdemo/demo/v1/";
             //"http://espressologic-msftdemo.cloudapp.net/rest/msftdemo/advwrk/v1/";
-            ConfigurationManager.AppSettings["APIKey"] = "demo_full";//"dJDGTz6f88cmu4kk1hpW";
+            ConfigurationManager.AppSettings["APIKey"] = "";//"";
         }
     }
 }
